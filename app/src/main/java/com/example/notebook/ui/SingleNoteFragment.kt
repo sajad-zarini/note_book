ca18901d780690857e73049f4d2e5a057ceaa781
Sajad <sajadzarini61@gmail.com>
2023-12-28 00:45:01 +0330

    complete ui of SingleNoteFragment.kt xml file and bottom app bar section on click handler color click migrate to app component ui from material 3

--- app/src/main/java/com/example/notebook/ui/SingleNoteFragment.kt
+++ app/src/main/java/com/example/notebook/ui/SingleNoteFragment.kt
@@ -13,6 +13,8 @@ class SingleNoteFragment : Fragment() {
 
     private lateinit var binding: FragmentSingleNoteBinding
 
+    private var savedColor = "#64C8FD"
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,6 +22,36 @@ class SingleNoteFragment : Fragment() {
         binding =
             DataBindingUtil.inflate(layoutInflater, R.layout.fragment_single_note, container, false)
 
+        binding.singleNote = this
+
         return binding.root
+    }
+
+    fun onColorViewClick(check: View) {
+        hideAllCheck()
+
+        check.visibility = View.VISIBLE
+
+        binding.apply {
+            when(check.id) {
+                this.check1.id -> savedColor = "#64C8FD"
+                this.check2.id -> savedColor = "#8069FF"
+                this.check3.id -> savedColor = "#FFCC36"
+                this.check4.id -> savedColor = "#D77FFD"
+                this.check5.id -> savedColor = "#FF419A"
+                this.check6.id -> savedColor = "#7FFB76"
+            }
+        }
+    }
+
+    private fun hideAllCheck() {
+        binding.apply {
+            this.check1.visibility = View.INVISIBLE
+            this.check2.visibility = View.INVISIBLE
+            this.check3.visibility = View.INVISIBLE
+            this.check4.visibility = View.INVISIBLE
+            this.check5.visibility = View.INVISIBLE
+            this.check6.visibility = View.INVISIBLE
+        }
     }
 }
